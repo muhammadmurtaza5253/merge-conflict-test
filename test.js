e69b6311cc42f96de90ba0fa43be679a1101605a
Muhammad Murtaza <muhammad.murtaza5253@gmail.com>
2024-10-14 12:07:11 +0500

    Initial commit in personal branch

--- test.js
+++ test.js
@@ -3,9 +3,9 @@
 var a = 5
 var b = 10
 
-if (a === b){
-    console.log('a is equal to b')
+if (a !== b){
+    console.log('a is not equal to b')
 }
 else{
-    console.log('a is not equal to b')
+    console.log('a is equal to b')
 }
